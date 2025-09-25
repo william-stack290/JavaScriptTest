cc1b10817bede9f75945ba42ca50664952964dc9
WillyUm <ekatzer@sjcawi.org>
2025-09-25 08:21:51 -0500

    Second Commit

---
+++ sphere.js
@@ -0,0 +1,10 @@
+var r = 5;
+var sa = 4 * pi * r**3;
+var pi = Math.PI;
+var v = 4/3 * pi * r**2;
+
+
+
+
+
+console.log("A sphere has a radius of " + r + ", a volume of " + v + " and a surface area of " + sa + ".");

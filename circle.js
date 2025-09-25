5ca0b48fedb7370552f4226459e0c98793af61a6
WillyUm <ekatzer@sjcawi.org>
2025-09-25 08:21:16 -0500

    first commit

---
+++ circle.js
@@ -0,0 +1,10 @@
+var r = 3445345;
+var pi = Math.PI
+var a = 2 * pi * r**2;
+var d = r * 2;
+var circum = pi * d;
+
+
+
+
+console.log("With a radius of " + r + ", the circle has an area of " + a + "and a circumference of " + circum + ".");
